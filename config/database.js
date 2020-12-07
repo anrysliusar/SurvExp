8a27cc628813b09f2cc2648961b72f5e146290df
Carambula <antibog6@gmail.com>
2020-12-07 22:09:36 +0200

    update tests

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     DATABASE_NAME: 'survexp',
     DATABASE_USERNAME: 'root',
-    DATABASE_PASSWORD: 'm1m2m3m4m5A',
+    DATABASE_PASSWORD: '12qwaszx',
     DATABASE_HOST: 'localhost',
     DATABASE_DRIVER: 'mysql',
 };
--- test/app/index.js
+++ test/app/index.js
@@ -20,11 +20,37 @@ module.exports = {
     run: (async () => {
             const usersList = await models.User.findAll();
             console.log(chalk.green(`Users list`));
-            console.log(toTable(usersList, "id", "name"));
+            console.log(toTable(usersList, "id", "name", "role_id"));
 
-            const projectsList = await models.Role.findAll();
-            console.log(chalk.green(`Projects List`));
-            console.log(toTable(projectsList, "id", "name"));
+            const rolesList = await models.Role.findAll();
+            console.log(chalk.green(`Roles List`));
+            console.log(toTable(rolesList, "id", "name"));
+
+            const expertsList = await models.Expert.findAll();
+            console.log(chalk.green(`Experts list`));
+            console.log(toTable(expertsList, "id", "occupation", "user_id"));
+
+            const answersList = await models.Answer.findAll();
+            console.log(chalk.green(`Answers list`));
+            console.log(toTable(answersList, "id", "date", "text", "expert_id", "question_id"));
+
+            const questionsList = await models.Question.findAll();
+            console.log(chalk.green(`Question list`));
+            console.log(toTable(questionsList, "id", "text", "survey_id", "questionType_id"));
+
+            const questionTypesList = await models.QuestionType.findAll();
+            console.log(chalk.green(`Question types list`));
+            console.log(toTable(questionTypesList, "id", "name"));
+
+            const possibleAnswersList = await models.PossibleAnswer.findAll();
+            console.log(chalk.green(`Possible answers list`));
+            console.log(toTable(possibleAnswersList, "id", "text", "question_id"));
+
+            const surveysList = await models.Survey.findAll();
+            console.log(chalk.green(`Surveys list`));
+            console.log(toTable(surveysList, "id", "name", "topic", "deadline"));
+
+
         }
     )
 };
